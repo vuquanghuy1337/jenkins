66c505486c023b147267fc3039e2a240ac9a9251
ubuntu <vuquanghuy1337@gmail.com>
2022-08-18 04:25:04 +0000

    Add dir

---
+++ jobDSL/Hello.groovy
@@ -0,0 +1,20 @@
+job('hello-world') {
+    steps {
+        shell('echo Hello World!')
+    }
+}
+
+pipelineJob('github-demo') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('vuquanghuy1337/jenkins')
+                    }
+                }
+            }
+            scriptPath('vars/sayHello.groovy')
+        }
+    }
+}
